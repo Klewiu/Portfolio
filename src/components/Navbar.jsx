cbd84b1f4afd686196ed4fe8015d4843a8ff9e8c
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-07-27 23:01:46 +0200

    Add icons / start navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] '>
+        
+    </div>
+  )
+}
