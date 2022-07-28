27ee53f35bee00b075265f48a14153aa444e4a7b
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-07-28 23:09:10 +0200

    Navbar edit:
    1. Add logo
    2. Add menu draft

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/logo2.png';
 
 export const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] '>
-        
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt='logo' style={{ width: '100px'}} />
+      </div>
+
+      {/* Menu */}
+      <ul className='hidden md:flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+
+      {/* Hamburger */}
+      <div className='hidden'>
+        <FaBars />
+      </div>
+
+      {/* Mobile Menu */}
+      <ul className='hidden'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className='hidden'></div>
+
     </div>
   )
 }
