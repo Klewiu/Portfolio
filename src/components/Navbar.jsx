462cbe406837e2f603205e69ab19e597715b35ca
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-07-29 18:19:58 +0200

    Finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import Logo from '../assets/logo2.png';
 
 export const Navbar = () => {
+
+  const [nav,setNav] = useState(false);
+  const handdleClick = () => setNav(!nav);
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -20,22 +24,22 @@ export const Navbar = () => {
 
 
       {/* Hamburger */}
-      <div className='hidden'>
-        <FaBars />
+      <div onClick = {handdleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile Menu */}
-      <ul className='hidden'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
       </ul>
 
       {/* Social Icons */}
       <div className='hidden'></div>
 
     </div>
-  )
-}
+  );
+}; 
