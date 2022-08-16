46d389fed9467a0d851a3acad2af72d187d79eb0
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-08-16 16:56:49 +0200

    Updates in work,jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,7 +10,7 @@ import portfolio from '../assets/porfolio_web2.JPG'
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] bg-fade'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] bg-fade py-5'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#ff9869ff]'>Work</p>
@@ -18,7 +18,7 @@ const Work = () => {
             </div>
 
             {/* Container */}
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-6'>
 
                 {/* Grid Item */}
 
@@ -94,32 +94,6 @@ const Work = () => {
 
                     
                 </div>
-                
-              {/* PORTFOLIO APP */}
-
-              <div 
-                style={{backgroundImage:`url(${portfolio})`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
-                    
-                    {/* hover effect */}
-
-                    <div className='invisible group-hover:visible'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                        REACT/TAILLWIND
-                        </span>
-                        <div className='pt-8 text center'>
-                            <a href='https://www.pasiekaradosc.pl/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/Klewiu/joyful_bees'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-
-                    
-                </div>
-
 
             </div>
             
