0e7b2e31624109d48063e9727088676f5577f37d
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-08-17 16:58:20 +0200

    Style change in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import obraz from "../assets/obraz.jpg";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300 pt-[100px]'">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
